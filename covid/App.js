defad6aeb326d5bc4b1921a197eec5638930669a
Sangeet Kumar <sangeetkumar@SigTuples-MacBook-Pro.local>
2020-03-29 14:25:05 +0530

    tried service not working

--- covid/App.js
+++ covid/App.js
@@ -2,15 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Registration from './component/Registration';
 import Location from './component/Location';
-import Service from './component/Service';
 
 
 export default class App extends React.Component {
 
   constructor() {
     super()
-    this.service = new Service();
-    this.service.startService();
   }
 
   render() {
