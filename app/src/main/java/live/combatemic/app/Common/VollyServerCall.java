23aa83c767987264e1b1eaecadbd11ca24b7a038
Sangeet Kumar <sangeetkumar@SigTuples-MacBook-Pro.local>
2020-04-21 09:28:35 +0530

    https call fix

--- app/src/main/java/live/combatemic/app/Common/VollyServerCall.java
+++ app/src/main/java/live/combatemic/app/Common/VollyServerCall.java
@@ -1,5 +1,6 @@
 package live.combatemic.app.Common;
 
+import android.annotation.SuppressLint;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.util.Log;
@@ -19,13 +20,25 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
 public class VollyServerCall {
 
-    private String BASE_URL = "http://combatemic.live/api/v1/";
+    private String BASE_URL = "https://combatemic.live/api/v1/";
     private static final String TAG = "VollyServerCall";
     private RequestQueue mRequestQueue;
 
     public void JsonObjectRequest(final Context myContext, final String MAIN_URL, final ServerCallback callback) {
+        handleSSLHandshake();
         mRequestQueue = Volley.newRequestQueue(myContext);
         final ProgressDialog pDialog = new ProgressDialog(myContext, R.style.AppCompatAlert);
         pDialog.setMessage("Loading...");
@@ -66,6 +79,7 @@ public class VollyServerCall {
     }
 
     public void JsonArrayRequest(final Context myContext, final String MAIN_URL, final ServerCallbackArray callback) {
+        handleSSLHandshake();
         mRequestQueue = Volley.newRequestQueue(myContext);
         final ProgressDialog pDialog = new ProgressDialog(myContext, R.style.AppCompatAlert);
         pDialog.setMessage("Loading...");
@@ -105,5 +119,40 @@ public class VollyServerCall {
         });
     }
 
+    /**
+     * Enables https connections
+     */
+    @SuppressLint("TrulyRandom")
+    public static void handleSSLHandshake() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+                @Override
+                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+
+                }
+
+                @Override
+                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
+
+                }
+
+                @Override
+                public X509Certificate[] getAcceptedIssuers() {
+                    return new X509Certificate[0];
+                }
+            }};
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+            HttpsURLConnection.setDefaultHostnameVerifier(new HostnameVerifier() {
+                @Override
+                public boolean verify(String arg0, SSLSession arg1) {
+                    return true;
+                }
+            });
+        } catch (Exception ignored) {
+        }
+    }
 
 }
